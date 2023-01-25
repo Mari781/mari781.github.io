abbea50e4fd7ae59357603907093c89399defc0a
DaMaria Abernathy <damariaabernathy@gmail.com>
2023-01-25 15:24:05 -0600

    webpage

--- JavaScript/script.js
+++
@@ -1,8 +0,0 @@
-/** typing animation **/
-var typed = new Typed(".typing",{
-    strings:["","Writer","Programmer","Designer","Creator"],
-    typeSpeed:140,
-    BackSpeed:120,
-    loop:true
-
-})
---
+++ script.js
@@ -0,0 +1,8 @@
+/** typing animation **/
+var typed = new Typed(".typing",{
+    strings:["","Writer","Programmer","Designer","Creator"],
+    typeSpeed:140,
+    BackSpeed:120,
+    loop:true
+
+})
