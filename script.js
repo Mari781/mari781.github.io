89260cfb0a9dae685820f844756812cfa640f624
DaMaria Abernathy <damariaabernathy@gmail.com>
2023-01-28 17:22:17 -0600

    project

---
+++ script.js
@@ -0,0 +1,8 @@
+/** typing animation **/
+var typed = new Typed(".typing",{
+    strings:["","Writer","Programmer","Designer","Creator"],
+    typeSpeed:140,
+    BackSpeed:600,
+    loop:true
+
+})
